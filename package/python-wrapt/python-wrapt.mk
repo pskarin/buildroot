79aa86f43b6cd9585646fea34d6d8aa591a63d1d
Per Skarin <per.skarin@ericsson.com>
2015-07-13 08:36:16 +0000

    Change wrapt version as exactly 1.10.2 is required

--- package/python-wrapt/python-wrapt.mk
+++ package/python-wrapt/python-wrapt.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_WRAPT_VERSION = 1.10.5
+PYTHON_WRAPT_VERSION = 1.10.2
 PYTHON_WRAPT_SOURCE = wrapt-$(PYTHON_WRAPT_VERSION).tar.gz
 PYTHON_WRAPT_SITE = http://pypi.python.org/packages/source/w/wrapt
 PYTHON_WRAPT_SETUP_TYPE = distutils
