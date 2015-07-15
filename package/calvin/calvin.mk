0865a7d436a9ed8cb53c14f4cf66a233341359a7
Per Skarin <per.skarin@ericsson.com>
2015-07-15 13:43:09 +0000

    Fix Calvin dep definition. Create development choice.

--- package/calvin/calvin.mk
+++ package/calvin/calvin.mk
@@ -9,6 +9,11 @@ CALVIN_SOURCE = calvin-$(CALVIN_VERSION).tar.gz
 CALVIN_SITE = http://skarin.mooo.com/
 CALVIN_LICENSE = Apache License Version 2
 CALVIN_LICENSE_FILES = LICENSE
+
+# Non-dev installation
+CALVIN_SETUP_TYPE = setuptools
+
+# Dev installation
 CALVIN_INSTALL_TARGET=YES
 
 define CALVIN_INSTALL_TARGET_CMDS
@@ -16,4 +21,9 @@ define CALVIN_INSTALL_TARGET_CMDS
 	rsync -a $(@D)/ $(TARGET_DIR)/root/calvin/
 endef
 
+ifeq ($(BR2_PACKAGE_CALVIN_DEV),y)
 $(eval $(generic-package))
+else
+$(eval $(python-package))
+endif
+
